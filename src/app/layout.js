724a7a75a4e2600933a6d3a0d1b3b38c69207a0b
Thibault You-Hout <youhoutthibault@gmail.com>
2023-06-15 14:36:59 +0200

    dark mode toggle

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
 import { Inter, Roboto } from "next/font/google";
 import Footer from "@/components/footer/Footer";
+import { ThemeProvider } from "../../context/ThemeContext";
 
 const inter = Inter({ subsets: ["latin"] });
 const roboto = Roboto({ subsets: ["latin"], weight: ["400"] });
@@ -15,11 +16,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={roboto.className}>
-        <div className="container">
-          <Navbar />
-          {children}
-          <Footer />
-        </div>
+        <ThemeProvider>
+          <div className="container">
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
